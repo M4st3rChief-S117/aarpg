07fcea48460ca64684c78c4155a562823bce2c84
ciprian <ciprian>
2026-06-22 16:01:29 +0200

    tiles and initial camera

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -18,8 +18,16 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	# direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	# direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	
+	# normalized, prevents the vector from having the magnitude > 1, so when moving diagonally it isn't faster
+	#direction = direction.normalized()
+	
+	direction = Vector2(
+		Input.get_axis("left", "right"),
+		Input.get_axis("up", "down")
+	).normalized()
 	pass
 
 func _physics_process(delta: float) -> void:
